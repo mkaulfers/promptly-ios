1908ab9f3f0f308b1ec24f1df74c00cfccd918b3
mkaulfers <matthew.kaulfers@yahoo.com>
2022-01-27 09:24:05 -0600

    main: Initial remote commit.

---
+++ Promptly/PromptlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PromptlyApp.swift
+//  Promptly
+//
+//  Created by Matthew Kaulfers on 1/27/22.
+//
+
+import SwiftUI
+
+@main
+struct PromptlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
